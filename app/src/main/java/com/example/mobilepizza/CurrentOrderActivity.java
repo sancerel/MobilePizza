69ebcbfe03099634033fadb1bf2fc8c527cddae2
C4rnivore <kirill.z13@mail.ru>
2023-05-14 14:50:52 +0500

    Добавил яндекс карту + мелкие изменения

---
+++ app/src/main/java/com/example/mobilepizza/CurrentOrderActivity.java
@@ -0,0 +1,44 @@
+package com.example.mobilepizza;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.yandex.mapkit.Animation;
+import com.yandex.mapkit.MapKitFactory;
+import com.yandex.mapkit.geometry.Point;
+import com.yandex.mapkit.map.CameraPosition;
+import com.yandex.mapkit.mapview.MapView;
+
+public class CurrentOrderActivity extends AppCompatActivity {
+    private final Point TARGET_LOCATION = new Point(56.844251, 60.653960);
+    private MapView mapView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        MapKitFactory.initialize(this);
+
+        setContentView(R.layout.activity_current_order);
+        mapView = (MapView)findViewById(R.id.mapview);
+        mapView.getMap().move(
+                new CameraPosition(TARGET_LOCATION, 11.0f, 0.0f, 0.0f),
+                new Animation(Animation.Type.SMOOTH, 0),
+                null);
+    }
+
+    @Override
+    protected void onStop() {
+        mapView.onStop();
+        MapKitFactory.getInstance().onStop();
+        super.onStop();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        MapKitFactory.getInstance().onStart();
+        mapView.onStart();
+    }
+}
--- app/src/main/java/com/example/mobilepizza/MainLKactivity.java
+++ app/src/main/java/com/example/mobilepizza/MainLKactivity.java
@@ -1,13 +1,13 @@
 package com.example.mobilepizza;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.transition.Visibility;
 
-import android.graphics.BlendModeColorFilter;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
-public class Main_LK_activity extends AppCompatActivity {
+public class MainLKactivity extends AppCompatActivity {
 
     private final String mainBtnColor = "#43AA72";
     private final String mainBtnColor_pressed = "#4A8E69";
@@ -18,5 +18,9 @@ public class Main_LK_activity extends AppCompatActivity {
         setContentView(R.layout.activity_main_lk);
         Button homeBtn = findViewById(R.id.lk_nav_home_btn);
 
+    }
+
+    public void SwitchToCurrent(View v){
+        startActivity(new Intent(this, CurrentOrderActivity.class));
     }
 }
